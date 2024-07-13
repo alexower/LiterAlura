bd6c870423bad0ba115fc7b0d24d3e2ef157959c
Alex M. Alavez <ower182.aw@gmail.com>
2024-07-12 20:29:08 -0600

    repository crated para inyection

--- src/main/java/com/alura/literatura/repository/AuthorRepository.java
+++ src/main/java/com/alura/literatura/repository/AuthorRepository.java
@@ -1,2 +1,15 @@
-package com.alura.literatura.repository;public interface AuthorRepository {
+package com.alura.literatura.repository;
+
+import com.alura.literatura.models.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorRepository  extends JpaRepository <Author,Long> {
+
+        Optional<Author> findByName(String name);
+
 }
+
--- src/main/java/com/alura/literatura/repository/BookRepository.java
+++ src/main/java/com/alura/literatura/repository/BookRepository.java
@@ -1,2 +1,16 @@
-package com.alura.literatura.repository;public interface BookRepository {
+package com.alura.literatura.repository;
+
+
+import com.alura.literatura.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
+
+    Optional<Book> findByTitle(String title);
+
+
 }
